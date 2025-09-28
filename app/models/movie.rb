4ca2afcd9bd4ba8ff475e03ea11bf29349894029
Tony Tian <jt3640@columbia.edu>
2025-09-28 13:04:04 +0000

    Filter the list of movies by rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+
+  # Return movies with selected ratings, or all if none given
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list.empty?
+      Movie.all
+    else
+      Movie.where(rating: ratings_list)
+    end
+  end
 end
